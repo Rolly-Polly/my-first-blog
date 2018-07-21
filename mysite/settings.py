eca7b1c1edbbc03672e3e38ab1b5e0be1ff63ce8
Rosangela <rosangelahockman@gmail.com>
2018-07-21 15:42:38 -0500

    upadate

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,7 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
-import os
+import os 
+os.environ[‘DJANGO_SETTINGS_MODULE’] = ‘settings’
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
